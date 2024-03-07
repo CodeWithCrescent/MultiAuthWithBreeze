413739c86a5de53247c0a8bc2eea1fcb54347e1f
Crescent Geniius <crescent.sambila@gmail.com>
2024-03-07 13:10:28 +0300

    Adding role foreign key colum from roles table in the users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->unsignedBigInteger('role_id');
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade')->deferrable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
@@ -27,6 +29,11 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('users', function (Blueprint $table) {
+            $table->dropForeign('lists_role_id_foreign');
+            $table->dropIndex('lists_role_id_index');
+            $table->dropColumn('role_id');
+        });
+
     }
 };
