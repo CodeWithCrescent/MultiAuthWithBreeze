c07d2758196ce018727040cb9376e9069189afd8
Crescent Geniius <crescent.sambila@gmail.com>
2024-03-07 14:20:23 +0300

    Creating Roles Seeder for Admin, Student and Teacher

---
+++ database/seeders/RolesSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\Models\Role;
+
+class RolesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $roles = [
+            'Admin',
+            'Teacher',
+            'Student',
+        ];
+
+        foreach ($roles as $role) {
+            Role::create([
+                'name' => $role,
+            ]);
+
+            // DB::table('roles')->insert([
+            //     'name' => 'Admin',
+            // ]);
+        }
+    }
+}
