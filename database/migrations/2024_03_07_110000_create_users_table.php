8beabc496277fc19b41e271da272e3c7041279af
Crescent Geniius <crescent.sambila@gmail.com>
2024-03-07 14:21:31 +0300

    Adding a 0 default value for role column

--- database/migrations/2024_03_07_110000_create_users_table.php
+++ database/migrations/2024_03_07_110000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->unsignedBigInteger('role_id');
+            $table->unsignedBigInteger('role_id')->default(3);
             $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
